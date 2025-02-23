e0888e706ffefac0ac6519354fe207698811f490
Eitan Turok <eitan.turok@gmail.com>
2025-02-23 11:25:34 +0200

    init

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs import play
+from icecream import ic
+
+load_dotenv()
+
+client = ElevenLabs(api_key=os.getenv('ELEVENLABS_API_KEY'))
+
+audio = client.text_to_speech.convert(
+    text="The first move is what sets everything in motion.",
+    voice_id="JBFqnCBsd6RMkjVDRZzb",
+    model_id="eleven_multilingual_v2",
+    output_format="mp3_44100_128",
+)
+
+play(audio)
